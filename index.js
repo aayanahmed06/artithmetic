57b18464b94c4f5865ceae41a09280a2c77f5e6e
Aani <93146995+aayanahmed06@users.noreply.github.com>
2022-03-10 21:10:54 +0500

    Damag kharab

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-console.log(`Hello world`)
+console.log(`Hello world`);
+
+//task
+
+let pkr = 340029
+const usd = 179.29
+
+let convert = pkr/usd
+
+console.log("Salary in usd =", convert);
+
+// task 2
+const days = 30
+let perday = pkr/days
+console.log( '7 days salary =', perday*27)
+
+
+// task 3 (pending)
+// math,urdu,english,sst,sience,isl,computer
+// 100,100,100,75,75,75,75//
+// obtain marks= 80,87,78,72,67,77,69//
+// grand total
+// percentage
+// avg
+let math = 80
+let urdu = 87
+let english = 78
+let isl  = 72
+let sst  = 67
+let sience = 77
+let computer = 69
+
+
+// obtain marks
+let obtainmarks = math + urdu + english + isl + sst +sience + computer
+
+console.log('obtain marks =', obtainmarks)
+
+// percentage
+const totalmarks = 100 + 100 + 100 + 100 + 100 + 100 + 100
+
+let percentage = (obtainmarks/totalmarks) * 100
+
+console.log('perecentage =',percentage)
+
+// Avg marks
+const subject = 7
+
+let avg = obtainmarks/subject
+console.log("avg marks are =", avg)
+
+
+// Find area and parameter of rectangle
+
+let a = 2
+let b = 4
+
+let p = 2*(a+b)
+
+console.log('p =', p)
+
+let area = a*b 
+
+console.log('area =', area)
+
+// Area of trianlge,square,c
+// Parameter of triangle square
+// Diameter of circle
+//
